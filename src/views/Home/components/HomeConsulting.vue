7e02c8ad1b1271784c01bb2286a68052c1b1ff3e
NicholasCui <cuikaihua2019@163.com>
2020-11-14 15:40:06 +0800

    fix: label

--- src/views/Home/components/HomeConsulting.vue
+++ src/views/Home/components/HomeConsulting.vue
@@ -5,33 +5,26 @@
       <div class="home__project__list">
         <div class="project">
           <p class="project__title">线上社区</p>
-          <p class="project__intro">706青年空间的线上社交平台</p>
+          <p class="project__intro">706青年空间的微型城市</p>
           <p class="project__detail">
-            有时我骑自行车去客户的办公室，有时我步行去MING工作室。根据我当天在哪里工作，我会路过著名的Eisbach河，人们一大早就在那里冲浪(即使是在冬天……嘿嘿)。当我走到办公室时，我穿过Viktualienmarkt食品市场，这里在一天的这个时候相当平静。
+            1913，博格丹诺夫作为列宁的党内同志和对手，出版了《组织构造学：一种通用的组织科学》，这本书直接影响了美国维纳撰写《控制论》。<br/>
+            1960年，《控制论》风靡美国，乔布斯当时也是小粉丝，当中国青年在相应号召上山下乡之际，美国青年人真忙着在大城市还有农村建立各种公社……
           </p>
           <p class="project__more" @click="showPayQrCode">支付￥20查看完整内容</p>
         </div>
         <div class="project">
           <p class="project__title">青年空间旗舰店</p>
-          <p class="project__intro">706青年空间的线上社交平台</p>
+          <p class="project__intro">706青年空间的新商业空间</p>
           <p class="project__detail">
-            有时我骑自行车去客户的办公室，有时我步行去MING工作室。根据我当天在哪里工作，我会路过著名的Eisbach河，人们一大早就在那里冲浪(即使是在冬天……嘿嘿)。当我走到办公室时，我穿过Viktualienmarkt食品市场，这里在一天的这个时候相当平静。
+            在移动互联网时代，随着话语权从几个固定的网站转移到无数分散的APP和小众文化平台，随着虚拟社交已经变成无数个小圈子的分层模式，对应的城市实体空间，也必然受到挑战。年轻人再也不喜欢去大商场了，那种光鲜亮丽的时尚服装，富丽堂皇的购物中心，琳琅满目的商品，那些庞然大物，这些和年轻人有什么关系呢……
           </p>
           <p class="project__more" @click="showPayQrCode">支付￥20查看完整内容</p>
         </div>
         <div class="project">
           <p class="project__title">生活实验室</p>
-          <p class="project__intro">706青年空间的线上社交平台</p>
+          <p class="project__intro">706青年空间的共享社区</p>
           <p class="project__detail">
-            有时我骑自行车去客户的办公室，有时我步行去MING工作室。根据我当天在哪里工作，我会路过著名的Eisbach河，人们一大早就在那里冲浪(即使是在冬天……嘿嘿)。当我走到办公室时，我穿过Viktualienmarkt食品市场，这里在一天的这个时候相当平静。
-          </p>
-          <p class="project__more" @click="showPayQrCode">支付￥20查看完整内容</p>
-        </div>
-        <div class="project">
-          <p class="project__title">虚拟城市</p>
-          <p class="project__intro">706青年空间的线上社交平台</p>
-          <p class="project__detail">
-            有时我骑自行车去客户的办公室，有时我步行去MING工作室。根据我当天在哪里工作，我会路过著名的Eisbach河，人们一大早就在那里冲浪(即使是在冬天……嘿嘿)。当我走到办公室时，我穿过Viktualienmarkt食品市场，这里在一天的这个时候相当平静。
+            显而易见是，今后10年，整个世界的年轻人对未来的工作方式，生活方式，以及社交模式的认知，都在改变，相应的，我们需要去符合这种社会趋势，创造出符合相应底层逻辑的产品，来满足这种社会需求……
           </p>
           <p class="project__more" @click="showPayQrCode">支付￥20查看完整内容</p>
         </div>
