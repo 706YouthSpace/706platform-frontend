27173ae91231d2b707d11449f859806a26dbf070
NicholasCui <cuikaihua2013@qq.com>
2021-03-27 22:51:39 +0800

    feat: recover project to origin version

--- src/views/Home/components/HomeJoin.vue
+++ src/views/Home/components/HomeJoin.vue
@@ -1,81 +1,40 @@
 <template>
   <div class="home__join">
-    <div class="home__join__bg"
-         ref="joinBg"
-         :style="{height: `${offsetLimit+100}%`, top: `-${bgOffsetY}%`}">
-      <img src="@/assets/images/Home/join.png"/>
-    </div>
+    <img class="home__join__bg" src="@/assets/images/Home/join.png"/>
     <div class="home__join__content">
       <p class="home__join__title">加入我们</p>
       <p class="home__join__para">与这个时代最具活力的青年人一起，探索生活的更多可能性</p>
       <p class="home__join__para">在众筹平台上支持706青年空间，并成为这项伟大事业的一部分</p>
-      <div class="home__join__btn home__join__btn--reg">注册</div>
+      <input class="home__join__input" type="text" placeholder="请输入邮箱"/>
       <div class="home__join__btn">参与众筹</div>
+      <div class="home__join__subscribe">
+        <input type="checkbox"/>订阅706青年空间的重要动态
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { throttle } from 'lodash'
-
 export default {
-  name: 'HomeJoin',
-  data () {
-    return {
-      offsetLimit: 20,
-      bgOffsetY: 0
-    }
-  },
-  created () {
-    document.addEventListener('scroll', this.throttleScrollEvent, false)
-  },
-  destroyed () {
-    document.removeEventListener('scroll', this.throttleScrollEvent, false)
-  },
-  methods: {
-    throttleScrollEvent () {
-      throttle(this.scrollEvent, 10)()
-    },
-    scrollEvent () {
-      const wHeight = document.documentElement.clientHeight
-      const { top, bottom } = this.$refs.joinBg.getBoundingClientRect()
-      if (top > wHeight) {
-        return
-      }
-      let yOffset = Math.floor(wHeight - top)
-      if (yOffset > bottom - top) {
-        yOffset = bottom - top
-      }
-      this.bgOffsetY = Math.floor(yOffset * this.offsetLimit / (bottom - top))
-    }
-  }
+  name: 'HomeJoin'
 }
 </script>
 
 <style scoped lang="less">
-  @import ~"@/assets/style/variables";
-
   .home__join {
+    display: flex;
+    align-items: center;
+    justify-content: center;
     position: relative;
     background: #fff;
     height: 400px;
-    padding-top: 47px;
-    box-sizing: border-box;
-    overflow: hidden;
 
     &__bg {
       position: absolute;
-      top: 0;
-      left: 0;
       width: 100%;
+      height: 100%;
+      object-fit: cover;
       z-index: 0;
-      transition: all .1s ease-in-out;
-      img {
-        display: block;
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-      }
     }
     &__content {
       width: 100%;
@@ -86,27 +45,41 @@ export default {
     }
     &__title {
       margin-bottom: 18px;
-      font-size: @--font-size-large;
+      font-size: 30px;
       font-weight: bold;
       text-align: center;
     }
     &__para {
-      line-height: 20px;
+      font-size: 12px;
+      line-height: 17px;
       text-align: center;
+    }
+    &__input {
+      width: 250px;
+      height: 33px;
+      margin-top: 44px;
+      padding: 11px;
+      box-sizing: border-box;
+      font-size: 12px;
+      outline: none;
+      border: none;
+      border-radius: 2px;
+      background: rgba(255, 255, 255, .28);
+      color: #fff;
+      &::placeholder {
+        color: rgba(255, 255, 255, .5);;
+      }
     }
     &__btn {
       border: 1px solid #fff;
       color: #fff;
-      width: 300px;
-      height: 40px;
-      margin: 17px auto 0 auto;
-      line-height: 40px;
-      font-size: 14px;
+      width: 250px;
+      height: 33px;
+      margin: 12px auto 0 auto;
+      line-height: 33px;
+      font-size: 12px;
       box-sizing: border-box;
       cursor: pointer;
-      &--reg {
-        margin-top: 50px;
-      }
     }
     &__subscribe {
       height: 27px;
